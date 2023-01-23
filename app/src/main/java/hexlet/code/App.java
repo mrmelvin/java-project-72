98a967dc787674dcc373e034cffb2ae9c594bc07
mike <korobov.mike@gmail.com>
2023-01-23 19:43:37 +0500

    Add simple Javalin app

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,18 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+
+public class App {
+
+
+    public static Javalin getApp() {
+        return Javalin.create()
+                .get("/", ctx -> ctx.result("Hello World"));
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start();
+    }
+}
