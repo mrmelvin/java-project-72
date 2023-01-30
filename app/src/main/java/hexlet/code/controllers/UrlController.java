4961dede8e41d2b3182ff3ddc32768a7174544e6
mike <korobov.mike@gmail.com>
2023-01-30 22:56:33 +0500

    Change landing

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -2,19 +2,43 @@ package hexlet.code.controllers;
 
 import hexlet.code.domain.UrlEntity;
 import hexlet.code.domain.query.QUrlEntity;
+import io.ebean.PagedList;
 import io.javalin.http.Context;
 
 import java.net.URL;
 import java.net.MalformedURLException;
 
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 import java.time.Instant;
 
 
 public class UrlController {
     public static void getAllUrls(Context context) {
-        List<UrlEntity> urlsList = new QUrlEntity().findList();
+
+        int page = context.queryParamAsClass("page", Integer.class).getOrDefault(1) - 1;
+        int rowsPerPage = 10;
+
+        PagedList<UrlEntity> pagedUrlEntity = new QUrlEntity()
+                                                    .setFirstRow(page * rowsPerPage)
+                                                    .setMaxRows(rowsPerPage)
+                                                    .orderBy()
+                                                    .id.asc()
+                                                    .findPagedList();
+
+        List<UrlEntity> urlsList = pagedUrlEntity.getList();
+
+        int lastPage = pagedUrlEntity.getTotalPageCount() + 1;
+        int currentPage = pagedUrlEntity.getPageIndex() + 1;
+        List<Integer> pages = IntStream
+                .range(1, lastPage)
+                .boxed()
+                .collect(Collectors.toList());
+
+        context.attribute("pages", pages);
+        context.attribute("currentPage", currentPage);
         context.attribute("urlsList", urlsList);
         context.render("urls/index.html");
     }
